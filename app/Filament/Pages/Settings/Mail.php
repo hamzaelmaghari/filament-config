28ba30b99ad62c596599cb2adf81a45948c90fb6
Hamza El Maghari <hoodpear@gmail.com>
2022-10-22 23:53:37 +0100

    first commit

---
+++ app/Filament/Pages/Settings/Mail.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Pages\Settings;
+
+use Filament\Pages\Page;
+
+class Mail extends Page
+{
+    protected static ?string $navigationIcon = 'heroicon-o-document-text';
+
+    protected static string $view = 'filament.pages.settings.mail';
+}
